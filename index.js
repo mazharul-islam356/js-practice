70f05edd52a505a186da5ded8b86a86dfc6d0b0c
Mazharul Islam <mazharulislam3569@gmail.com>
2026-04-26 23:38:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const student = [
+  { name: "sifat", age: 20 },
+  { name: "rahim", age: 22 },
+  { name: "rahim", age: 22 },
+  { name: "rahim", age: 22 },
+  { name: "rahim", age: 22 },
+  { name: "rahim", age: 22 },
+  { name: "rahim", age: 22 },
+  { name: "rahim", age: 22 },
+];
+
+console.table(student);
