19f7415ac25f6e1173ece63c01051cd770edc471
Mazharul Islam <mazharulislam3569@gmail.com>
2026-04-27 23:58:13 +0600

    add condition function

--- index.js
+++ index.js
@@ -1,12 +1,66 @@
-const student = [
-  { name: "sifat", age: 20 },
-  { name: "rahim", age: 22 },
-  { name: "rahim", age: 22 },
-  { name: "rahim", age: 22 },
-  { name: "rahim", age: 22 },
-  { name: "rahim", age: 22 },
-  { name: "rahim", age: 22 },
-  { name: "rahim", age: 22 },
-];
+// else if condition
 
-console.table(student);
+// let day = 3;
+
+// if (day === 1) {
+//   console.log("sataruday");
+// } else if (day === 2) {
+//   console.log("sunday");
+// } else if (day === 3) {
+//   console.log("monday");
+// } else {
+//   console.log("invalid day");
+// }
+
+// switch
+
+// let day = 1;
+
+// switch (day) {
+//   case 1:
+//     console.log("satarday");
+//     break;
+//   case 2:
+//     console.log("sunday");
+//     break;
+//   case 3:
+//     console.log("monday");
+//     break;
+//   default:
+//     console.log("invalid day");
+// }
+
+// loop
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log("number", i);
+// }
+
+// let password = "";
+// const correctPassword = "magic";
+
+// while (password !== correctPassword) {
+//   password = prompt("Enter the secret password:");
+
+//   if (password !== correctPassword) {
+//     alert("Wrong! Try again.");
+//   }
+// }
+
+// alert("Access Granted!");
+
+// normal function
+
+function add(a, b, c) {
+  return a + b - c;
+}
+
+// console.log(add(5, 7, 2));
+
+// array function
+const multiply = (a, b) => {
+  let result = a + b;
+  return result;
+};
+
+console.log(multiply(2, 8));
