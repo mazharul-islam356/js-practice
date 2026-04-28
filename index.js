948280e576c5bdac6aa84edb5e11ed000d0942f1
Mazharul Islam <mazharulislam3569@gmail.com>
2026-04-28 22:45:33 +0600

    add loop

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
 // else if condition
 
-// let day = 3;
+let day = 3;
 
-// if (day === 1) {
-//   console.log("sataruday");
-// } else if (day === 2) {
-//   console.log("sunday");
-// } else if (day === 3) {
-//   console.log("monday");
-// } else {
-//   console.log("invalid day");
-// }
+if (day === 1) {
+  console.log("sataruday");
+} else if (day === 2) {
+  console.log("sunday");
+} else if (day === 3) {
+  console.log("monday");
+} else {
+  console.log("invalid day");
+}
 
 // switch
 
-// let day = 1;
+let day = 1;
 
-// switch (day) {
-//   case 1:
-//     console.log("satarday");
-//     break;
-//   case 2:
-//     console.log("sunday");
-//     break;
-//   case 3:
-//     console.log("monday");
-//     break;
-//   default:
-//     console.log("invalid day");
-// }
+switch (day) {
+  case 1:
+    console.log("satarday");
+    break;
+  case 2:
+    console.log("sunday");
+    break;
+  case 3:
+    console.log("monday");
+    break;
+  default:
+    console.log("invalid day");
+}
 
 // loop
 
-// for (let i = 1; i <= 5; i++) {
-//   console.log("number", i);
-// }
+for (let i = 1; i <= 5; i++) {
+  console.log("number", i);
+}
 
-// let password = "";
-// const correctPassword = "magic";
+let password = "";
+const correctPassword = "magic";
 
-// while (password !== correctPassword) {
-//   password = prompt("Enter the secret password:");
+while (password !== correctPassword) {
+  password = prompt("Enter the secret password:");
 
-//   if (password !== correctPassword) {
-//     alert("Wrong! Try again.");
-//   }
-// }
+  if (password !== correctPassword) {
+    alert("Wrong! Try again.");
+  }
+}
 
-// alert("Access Granted!");
+alert("Access Granted!");
 
 // normal function
 
